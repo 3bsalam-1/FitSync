74a21db51a3fd60d440511419e2b680982180d50
NadaAshrafMuhammad <monstergirl4480@gmail.com>
2024-01-27 07:04:47 +0200

    making login screen

--- Mobile/fitsync/lib/main.dart
+++ Mobile/fitsync/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitsync/screens/Login/login_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'cubits_logic/cubit/bluetooth_cubit.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: BlocProvider(
         create: (context) => BluetoothCubit()..checkBluetoothConnectivity(),
-        child: const SurveyScreen(),
+        child: const loginPage(),
       ),
     );
   }
