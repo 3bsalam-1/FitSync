7f4cd7287b265d3f81898edcd3ce8a1bde287d85
nada <nada50tarek@gmail.com>
2024-01-29 19:52:06 +0200

    create list_question_item.dart which is the implementation of the card of the question choices refactor choice_questions.dart which is remove child of the listView.seperated() & add the listQuestionItem()

--- Mobile/fitsync/lib/screens/survey/survey_screen.dart
+++ Mobile/fitsync/lib/screens/survey/survey_screen.dart
@@ -1,3 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../cubits_logic/cubit/animated_list_view.dart';
+import '../../cubits_logic/cubit/choise_questions_cubit.dart';
 import '../../shared/widgets/survey_comp/choice_questions.dart';
 import '../../shared/widgets/survey_comp/first_name_question.dart';
 import 'package:flutter/material.dart';
@@ -10,24 +14,34 @@ class SurveyScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: (){
-            //TODO here
-          }, 
-          icon: const Icon(
-            Icons.arrow_circle_left,
-            color: purple3,
-            size: 40,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ChoiseQuestionsCubit(),
+        ),
+        BlocProvider(
+          create: (context) => AnimatedListView()..startAnimation(),
+        ),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: (){
+              //TODO here
+            }, 
+            icon: const Icon(
+              Icons.arrow_circle_left,
+              color: purple3,
+              size: 40,
+            ),
           ),
         ),
-      ),
-      backgroundColor: white,
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        // TODO here
-        child: ChoiceQuestion() // WeightQuestion() //AgeQuestion(), //FirstNameQuestion(),
+        backgroundColor: white,
+        body: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          // TODO here
+          child: ChoiceQuestion() // WeightQuestion() //AgeQuestion(), //FirstNameQuestion(),
+        ),
       ),
     );
   }
--- Mobile/fitsync/lib/shared/widgets/survey_comp/choice_questions.dart
+++ Mobile/fitsync/lib/shared/widgets/survey_comp/choice_questions.dart
@@ -4,6 +4,7 @@ import '../../../cubits_logic/cubit/choise_questions_cubit.dart';
 import '../../question_survey.dart';
 import 'package:flutter/material.dart';
 import '../../colors/colors.dart';
+import 'list_question_item.dart';
 
 class ChoiceQuestion extends StatefulWidget {
   const ChoiceQuestion({super.key});
@@ -17,22 +18,16 @@ class _ChoiceQuestionState extends State<ChoiceQuestion> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => ChoiseQuestionsCubit(),
-          ),
-          BlocProvider(
-            create: (context) => AnimatedListView()..startAnimation(),
-          ),
-        ],
-        child: BlocBuilder<AnimatedListView, bool>(
-          builder: (_, isStarted) =>
-              BlocBuilder<ChoiseQuestionsCubit, List<int>>(
-            builder: (context, indexQuestion) => Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
+      child: BlocBuilder<AnimatedListView, bool>(
+        builder: (_, isStarted) => BlocBuilder<ChoiseQuestionsCubit, List<int>>(
+          builder: (context, indexQuestion) => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AnimatedOpacity(
+                // if the animation started show the text
+                opacity: isStarted ? 1 : 0,
+                duration: const Duration(milliseconds: 800),
+                child: Text(
                   // index = 0 for the question index
                   questionSurvey[indexQuestion[0]].question,
                   style: const TextStyle(
@@ -41,66 +36,35 @@ class _ChoiceQuestionState extends State<ChoiceQuestion> {
                     fontWeight: FontWeight.w600,
                   ),
                 ),
-                const SizedBox(height: 60),
-                ListView.separated(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: questionSurvey[indexQuestion[0]].choice.length,
-                  itemBuilder: (_, index) => InkWell(
-                    onTap: () {
-                      context
-                          .read<ChoiseQuestionsCubit>()
-                          //         question number  , answer
-                          .isSelected(indexQuestion[0], index);
-                      Future.delayed(const Duration(milliseconds: 500), () {
-                        
-                        context.read<AnimatedListView>().stopAnimation();
-                      });
-                      Future.delayed(const Duration(seconds: 2), () {
-                        context.read<ChoiseQuestionsCubit>().nextQuestion(index);
-                        context.read<AnimatedListView>().startAnimation();
-                      });
-                    },
-                    borderRadius: BorderRadius.circular(19),
-                    child: AnimatedContainer(
-                      curve: Curves.easeInOut,
-                      transform:
-                          Matrix4.translationValues(isStarted ? 0 : 2000, 0, 0),
-                      duration: Duration(milliseconds: 800 + (200 * index)),
-                      width: double.infinity,
-                      padding: const EdgeInsets.only(
-                        left: 19,
-                        top: 33,
-                        bottom: 33,
-                      ),
-                      decoration: BoxDecoration(
-                        // index = 1 for the answer checking if it selected or NOT
-                        color: indexQuestion[1] == index ? purple2 : white,
-                        borderRadius: BorderRadius.circular(19),
-                        boxShadow: [
-                          BoxShadow(
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: const Offset(0, 3),
-                            color: black2.withOpacity(0.2),
-                          ),
-                        ],
-                      ),
-                      child: Text(
-                        questionSurvey[indexQuestion[0]].choice[index],
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                          // index = 1 for the answer checking if it selected or NOT
-                          color: indexQuestion[1] == index ? white : black,
-                        ),
-                      ),
-                    ),
-                  ),
-                  separatorBuilder: (_, __) => const SizedBox(height: 20),
+              ),
+              const SizedBox(height: 60),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: questionSurvey[indexQuestion[0]].choice.length,
+                itemBuilder: (_, index) => InkWell(
+                  onTap: () {
+                    //                                            question number  , answer
+                    context.read<ChoiseQuestionsCubit>().isSelected(indexQuestion[0], index);
+                    Future.delayed(const Duration(milliseconds: 500), () {
+                      context.read<AnimatedListView>().stopAnimation();
+                    });
+                    Future.delayed(const Duration(seconds: 2), () {
+                      context.read<ChoiseQuestionsCubit>().nextQuestion(index);
+                      context.read<AnimatedListView>().startAnimation();
+                    });
+                  },
+                  borderRadius: BorderRadius.circular(19),
+                  child: ListQuestionItem(
+                    index: index, 
+                    indexQuestion: indexQuestion, 
+                    isStarted: isStarted, 
+                    questionSurvey: questionSurvey,
+                  ), 
                 ),
-              ],
-            ),
+                separatorBuilder: (_, __) => const SizedBox(height: 20),
+              ),
+            ],
           ),
         ),
       ),
