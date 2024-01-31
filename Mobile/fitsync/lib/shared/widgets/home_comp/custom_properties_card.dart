e0c8079da27ef54de93518c2d49e4ecfff874e24
NadaAshrafMuhammad <monstergirl4480@gmail.com>
2024-01-31 07:53:54 +0200

    finish verification screen

---
+++ Mobile/fitsync/lib/shared/widgets/home_comp/custom_properties_card.dart
@@ -0,0 +1,56 @@
+
+import 'package:fitsync/shared/colors/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomPropertiesCard extends StatelessWidget {
+  final String label1;
+  final String label2;
+   CustomPropertiesCard({
+    required this.label1,
+    required this.label2,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 95,
+      height: 65,
+      decoration: BoxDecoration(
+          color: white,
+          borderRadius: BorderRadius.circular(19),
+          boxShadow: [
+            BoxShadow(
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
+              color: black2.withOpacity(0.15),
+            ),
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            label1,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: black4,
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Text(
+            label2,
+            style: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: gray4,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
