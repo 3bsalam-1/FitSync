ba39533e4fcc806fc06a81ea1c740f16e0ff8346
Mohamed-Elsayed-1 <mohamedelsayed55086@gmail.com>
2024-03-01 17:32:51 +0200

    Update routes for authentication.
    - Change the success redirect route for Google authentication from "/" to "/home". (generated by blackbox.ai )

--- Back-End/app.js
+++ Back-End/app.js
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
 
-app.use("/", (req, res, next) => {
+app.use("/home", (req, res, next) => {
   res.send("<h1>home screen</h1>");
 });
 
